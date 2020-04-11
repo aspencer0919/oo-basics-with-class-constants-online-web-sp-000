4738d0be2562ae650d3bd981e17c7e30f4caa9c3
Adam Spencer <aspencer0919@gmail.com>
2020-04-11 05:11:47 +0000

    Automatically backed up by Learn

--- lib/shoe.rb
+++ lib/shoe.rb
@@ -1,14 +1,19 @@
 class Shoe
-  attr_accessor :color, :size, :material, :condition
-  attr_reader :brand
+  attr_accessor :author, :page_count, :genre
+  attr_reader :title
 
-  def initialize(brand)
-    @brand = brand
+  GENRES = []
+
+  def initialize(title)
+    @title = title
   end
 
-  def cobble
-    self.condition = "new"
-    puts "Your shoe is as good as new!"
+  def genre=(genre)
+    @genre = genre
+    GENRES << genre
   end
 
+  def turn_page
+    puts "Flipping the page...wow, you read fast!"
+  end
 end
